2ba3726a4b6eaade8978ce86378f56d4febba0ae
unknown <adula26@uni.coventry.co.uk>
2019-11-19 14:28:53 +0000

    Inserted Carousel in the home page

---
+++ public/scripts/home.js
@@ -0,0 +1,98 @@
+const track = document.querySelector('.Carousel__track');
+const slides = Array.from(track.children);
+const slideWidth = slides[0].getBoundingClientRect().width;
+const nextButton = document.querySelector('.Carousel__button--right');
+const prevButton = document.querySelector('.Carousel__button--left');
+const dotsNav = document.querySelector('.Carousel__nav');
+const dots = Array.from(dotsNav.children);
+
+// Arrange slides next to each other
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
+// click left, move slides left
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+});
+
+// click right, move slides right
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+});
+
+// click nav indicators, move to particular slide
+dotsNav.addEventListener('click', e => {
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot)
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+})
+
+/*
+HTML:
+ <li class="Carousel__slide" onmouseover="showExtras()" onmouseout="hideExtras()">
+
+
+JS:
+const Capt = document.querySelector('.caption');
+const sNum = document.querySelector('.slideNum');
+
+function showExtras() {
+    Capt.classList.remove('is-hidden');
+    sNum.classList.remove('is-hidden');
+}
+function hideExtras() {
+    Capt.classList.add('is-hidden');
+    sNum.classList.add('is-hidden');
+}
+*/
