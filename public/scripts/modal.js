cda882ca52fae6356f6fc001869bca965cc8337b
hopkin70 <hopkin70@coventry.ac.uk>
2019-12-03 11:45:09 +0000

    Updated modal.js to grab the orderId

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,6 +1,19 @@
-function modal() {
-    let close = document.getElementById('close');
-    let modal = document.getElementById('modal');
+for (i = 0; i < document.getElementsByClassName('orderBtn').length; i++) {
+    document.getElementsByClassName('orderBtn')[i].onclick = modal;
+}
 
-    modal.classList.toggle('d-modal');
+function modal(clicked) {
+    let close = document.getElementById('close');
+    let modalElem = document.getElementById('modal');
+    let elemId = document.getElementById('orderId');
+    if (!modalElem.classList.contains('d-modal')) {
+        let orderId = clicked.target;
+        orderId = orderId.parentElement.children[0].innerHTML.slice(10);
+        elemId.value = orderId;
+        elemId.innerHTML = orderId;
+        elemId.textContent = orderId;
+    }
+
+
+    modalElem.classList.toggle('d-modal');
 }
